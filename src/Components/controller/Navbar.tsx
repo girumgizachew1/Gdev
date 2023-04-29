60aeb5187aa9f12a586f51ca61f6b4b5eeb97e8c
girumgizachew1 <girumegizachew@gmail.com>
2023-04-29 20:28:00 +0300

    basic layout

--- src/Components/controller/Navbar.tsx
+++ src/Components/controller/Navbar.tsx
@@ -6,44 +6,63 @@ import { BsFillPlayFill } from 'react-icons/bs';
 import { BiRefresh } from 'react-icons/bi';
 import { IoArrowRedoOutline } from 'react-icons/io5';
 import { IoArrowUndoOutline } from 'react-icons/io5';
-
 import { FiCodepen } from 'react-icons/fi';
 import { CgScreenWide } from 'react-icons/cg';
 import { CgScreen } from 'react-icons/cg';
 import { SlScreenTablet } from 'react-icons/sl';
 import { SlScreenSmartphone } from 'react-icons/sl';
-
-
-FiCodepen
+import { setWidth , selectWidth } from '@/Redux/width/screenwidth';
+import { useDispatch, useSelector } from 'react-redux'
 function Navbar() {
+    const dispatch = useDispatch()
+    const width = useSelector(selectWidth)
+    
+    const handlePhoneClick = () =>{
+        dispatch(setWidth(576))
+    }
+    
+    const handleTabletClick = () =>{
+        dispatch(setWidth(991))
+    }
+    
+    const handlePcScreen1Click = () =>{
+        dispatch(setWidth(1259))
+    }
+    
+    const handlePcScreen2Click = () =>{
+        dispatch(setWidth(1960))
+    }
+    
+
+
     return (
         <div className='flex items-center justify-between text-zinc-300 text-sm px-6' >
             <div className='flex space-x-10' >
                 <div className='flex space-x-4 text-base' >
                     <h1 className='my-3 text-zinc-300 text-sm rounded-sm' >Project:</h1>
-                    <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><BsFillCloudArrowUpFill /></a>
-                    <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><FiCodepen /></a>
-                    <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><MdDelete /></a>
+                    <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm' ><BsFillCloudArrowUpFill /></a>
+                    <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm' ><FiCodepen /></a>
+                    <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm' ><MdDelete /></a>
                 </div>
                 <div className='flex space-x-4 text-base' >
                     <h1 className='my-3 text-zinc-300 text-sm' >Display:</h1>
                     <button className='bg-zinc-700 px-3 my-2 rounded-sm text-xs' >Html</button>
                     <button className='bg-zinc-700 px-3 my-2 rounded-sm text-xs' >Css</button>
                     <button className='bg-zinc-700 px-3 my-2 rounded-sm text-xs' >Code</button>
-                    <a className='bg-zinc-700 px-3 my-2 rounded-sm ' ><MdLightMode /></a>
-                    <a className='bg-zinc-700 px-3 my-2 rounded-sm ' ><BsFillPlayFill /></a>
+                    <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm ' ><MdLightMode /></a>
+                    <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm ' ><BsFillPlayFill /></a>
                 </div>
             </div>
             <div className='flex space-x-2' >
             <h1 className='my-3 text-zinc-300 text-sm' >Screen:</h1>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><SlScreenSmartphone/></a>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><SlScreenTablet /></a>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm ' ><CgScreen /></a>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm ' ><CgScreenWide /></a>
+                <a onClick={handlePhoneClick} className={`px-3 my-3 pt-1 rounded-sm ${width < 768 && width >= 320 ? " bg-orange-400 text-zinc-800" :"bg-zinc-700"} `} ><SlScreenSmartphone/></a>
+                <a onClick={handleTabletClick}  className={`px-3 my-3 pt-1 rounded-sm ${width < 992 && width >= 768 ? " bg-orange-400 text-zinc-800" :"bg-zinc-700"}`} ><SlScreenTablet /></a>
+                <a onClick={handlePcScreen1Click} className={`px-3 my-3 pt-1 rounded-sm  ${width < 1260 && width >= 992 ? " bg-orange-400 text-zinc-800" :"bg-zinc-700"}`} ><CgScreen /></a>
+                <a onClick={handlePcScreen2Click} className={`px-3 my-3 pt-1 rounded-sm  ${width < 1961 && width >= 1260 ? " bg-orange-400 text-zinc-800" :"bg-zinc-700"} `} ><CgScreenWide /></a>
                 <h1 className='my-3 text-zinc-300 text-sm' >control:</h1>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><BiRefresh/></a>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm' ><IoArrowUndoOutline /></a>
-                <a className='bg-zinc-700 px-3 my-2 rounded-sm ' ><IoArrowRedoOutline /></a>
+                <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm' ><BiRefresh/></a>
+                <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm' ><IoArrowUndoOutline /></a>
+                <a className='bg-zinc-700 px-3 my-3 pt-1 rounded-sm ' ><IoArrowRedoOutline /></a>
                 <h1 className='p-2 text-zinc-300' >user:</h1>
                 <button className='bg-zinc-700 px-3 my-2 rounded-sm' >Login</button>
                 <button className='bg-zinc-700 px-3 my-2 rounded-sm' >Register</button>
